08392b59266720cd523ce215fa56b00a1a3863e9
Babakailash <nathkailash2020@gmail.com>
2021-05-03 23:58:22 +0530

    Added:Valid And Invalid Email Test

--- EmailRegex.java
+++ EmailRegex.java
@@ -16,15 +16,29 @@ public class EmailRegex
    emails.add("abc@1.com");
    emails.add("abc@gmail.com.com");
    emails.add("abc+100@gmail.com");
- 
-    //Invalid emails
-   emails.add("abc#yahoo.com");
-   emails.add("@yahoo.com");
- 
-   String regex = "^(.+)@(.+)$";
- 
+
+    //Invalid emails test
+   emails.add("abc"); //must contains"@" symbol
+   emails.add("abc@.com.my");
+   emails.add("abc123@gmail.a");
+   emails.add("abc123@.com");
+   emails.add("abc123@.com.com");
+   emails.add(".abc@abc.com");
+   emails.add("abc()*@gmail.com");
+   emails.add("abc@%*.com");
+   emails.add("abc..2002@gmail.com");
+   emails.add("abc.@gmail.com");
+   emails.add("abc@abc@gmail.com");
+   emails.add("abc@gmail.com.1a");
+   emails.add("abc@gmail.com.aa.au");
+
+
+//String regex = "^(.+)@(.+)$";
+//String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+ //String regex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+   String regex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
    Pattern pattern = Pattern.compile(regex);
- 
+
    for(String email : emails){
        Matcher matcher = pattern.matcher(email);
    System.out.println(email +" : "+ matcher.matches());
