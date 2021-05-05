62cb8f8a8371ede6d5d7f006c71747ccd50a6d60
Babakailash <nathkailash2020@gmail.com>
2021-05-05 14:40:20 +0530

    Added:Password Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,20 +3,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	private static final String PATTERN = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+	private static final String PATTERN = "^(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$";
 	public static void main(String args[]){ 
 	    
 		Scanner c = new Scanner(System.in);
-		System.out.println("Eneter the Email: ");
+		System.out.println("Eneter Password: ");
 		String values = c.next();
  
 		Pattern pattern = Pattern.compile(PATTERN);
 		Matcher matcher = pattern.matcher(values);
 		if(matcher.matches()){
-			System.out.println("UserEmail "+ values +" is valid");
+			System.out.println("User Password "+ values +" is valid");
 		}else {
-			System.out.println("UserEmail "+ values +" is invalid");
+			System.out.println("User Password "+ values +" is invalid");
 		 
 		}
+
 	}
- }
+}
