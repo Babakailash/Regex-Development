3b101fd9fe6312bdf3f6d410a7fdb636f76d6f73
Babakailash <nathkailash2020@gmail.com>
2021-05-05 13:26:22 +0530

    Added:Email Validation Pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,22 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-	
-	private static final String PATTERN = "^[A-Z][a-z]{2,}$";
+	private static final String PATTERN = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
 	public static void main(String args[]){ 
 	    
 		Scanner c = new Scanner(System.in);
-		System.out.println("Eneter the Last Name: ");
+		System.out.println("Eneter the Email: ");
 		String values = c.next();
  
 		Pattern pattern = Pattern.compile(PATTERN);
 		Matcher matcher = pattern.matcher(values);
 		if(matcher.matches()){
-			System.out.println("Username "+ values +" is valid");
+			System.out.println("UserEmail "+ values +" is valid");
 		}else {
-			System.out.println("Username "+ values +" is invalid");
-		}		  
+			System.out.println("UserEmail "+ values +" is invalid");
+		 
+		}
 	}
-}
+ }
